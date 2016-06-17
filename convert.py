c8ab84407ac7864b606b4c6f3bf9ece69604243e
Evan Porter <evan.porter@dotman.ca>
2016-06-17 18:03:33 -0300

    break out function to handle indefinite number of columns

--- convert.py
+++ convert.py
@@ -1,3 +1,7 @@
+def create_key_value_json(key, value):
+    return '"{0}": "{1}"'.format(key, value)
+
+
 def print_json(csv_file_name):
     with open(csv_file_name) as csvFile:
         lines = csvFile.readlines()
@@ -8,7 +12,12 @@ def print_json(csv_file_name):
 
         for line in lines[1:]:
             values = line.strip().split(",")
-            print('{{"{0}": "{1}","{2}": "{3}","{4}": "{5}", "{6}": "{7}"}},'.format(props[0], values[0], props[1], values[1], props[2], values[2], props[3], values[3]))
+
+            json_chunks = []
+            for index in range(len(values)):
+                json_chunks.append(create_key_value_json(props[index], values[index]))
+
+            print('{{{0}}}'.format(', '.join(json_chunks)))
 
         print("]")
 
