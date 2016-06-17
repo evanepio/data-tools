a2dfc56ebfd5297aaaf1c829b322dd3f12ac7d16
Evan Porter <evan.porter@dotman.ca>
2016-06-17 17:51:30 -0300

    Move to function, have standard Python script beginning

--- convert.py
+++ convert.py
@@ -1,12 +1,17 @@
-with open('historic_site_visits.csv') as csvFile:
-    lines = csvFile.readlines()
+def print_json(csv_file_name):
+    with open(csv_file_name) as csvFile:
+        lines = csvFile.readlines()
 
-    props = lines[0].strip().split(',')
+        props = lines[0].strip().split(',')
 
-    print("[")
+        print("[")
 
-    for line in lines[1:]:
-        values = line.strip().split(",")
-        print('{{"{0}": "{1}","{2}": "{3}","{4}": "{5}", "{6}": "{7}"}},'.format(props[0], values[0], props[1], values[1], props[2], values[2], props[3], values[3]))
+        for line in lines[1:]:
+            values = line.strip().split(",")
+            print('{{"{0}": "{1}","{2}": "{3}","{4}": "{5}", "{6}": "{7}"}},'.format(props[0], values[0], props[1], values[1], props[2], values[2], props[3], values[3]))
 
-    print("]")
+        print("]")
+
+
+if __name__ == '__main__':
+    print_json('historic_site_visits.csv')
