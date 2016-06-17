65039c2878b958de61b14d88bb3fb0b98ad705d1
Evan Porter <evan.porter@dotman.ca>
2016-06-17 19:33:15 -0300

    Refactoring a little

--- convert.py
+++ convert.py
@@ -12,22 +12,20 @@ def create_single_object_json(keys, line):
     return '{{{0}}}'.format(', '.join(json_chunks))
 
 
-def print_json(csv_file_name):
+def csv_file_to_json_array(csv_file_name):
+    individual_obj = []
+
     with open(csv_file_name) as csvFile:
         lines = csvFile.readlines()
 
         props = lines[0].strip().split(',')
 
-        print("[")
-
-        individual_obj = []
         for line in lines[1:]:
             individual_obj.append(create_single_object_json(props, line))
 
-        print(',\n'.join(individual_obj))
-
-        print("]")
+    return '[' + ',\n'.join(individual_obj) + ']'
 
 
 if __name__ == '__main__':
-    print_json('historic_site_visits.csv')
+    json_array = csv_file_to_json_array('historic_site_visits.csv')
+    print(json_array)
