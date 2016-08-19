dde75c5e4aa8afae28d6c6fd0548ef765cff0d95
Evan Porter <evan.porter@dotman.ca>
2016-08-18 22:42:19 -0300

    Rename methods and variables

--- convertCSVtoJSON.py
+++ convertCSVtoJSON.py
@@ -1,16 +1,16 @@
 import sys, json, csv
 
 
-def get_rows_from_csv_file(csv_file_name):
+def get_keys_and_values_list(csv_file_name):
     all_rows = []
     with open(csv_file_name) as csv_file:
         reader = csv.reader(csv_file, dialect='excel')
         for row in reader:
             all_rows.append(row)
-    return all_rows
+    return all_rows[0], all_rows[1:]
 
 
 if __name__ == '__main__':
-    rows = get_rows_from_csv_file(sys.argv[1])
-    objects = [dict(zip(rows[0], current_row)) for current_row in rows[1:]]
+    keys, values_list = get_keys_and_values_list(sys.argv[1])
+    objects = [dict(zip(keys, values)) for values in values_list]
     print(json.dumps(objects, indent=4, separators=(',', ': ')))
